5b1a4fc1376e112defb199a3f2160af3c3285445
Muftullo <mufasadullaev@gmail.com>
2022-06-17 20:13:46 +0500

    initial app start

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+const fs = require("fs");
 
+//setting pug for frontend
+app.set("view engine", "pug");
+app.use("/static", express.static("public"));
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+
+app.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log(`Running ${PORT}`);
+});
