4af60cdf460115742aad8de9d040732c220d830b
Muftullo <mufasadullaev@gmail.com>
2022-06-17 20:17:15 +0500

    crud functionality

--- app.js
+++ app.js
@@ -11,6 +11,105 @@ app.use(
     extended: false,
   })
 );
+//get the existing note
+app.get("/", (req, res) => {
+  fs.readFile("./data/lesson.json", (err, data) => {
+    if (err) throw err;
+
+    const lessons = JSON.parse(data);
+
+    res.render("home", { lessons: lessons });
+  });
+});
+//add function for notes
+app.post("/add", (req, res) => {
+  const formData = req.body;
+
+  if (formData.lesson.trim() == "") {
+    fs.readFile("./data/lesson.json", (err, data) => {
+      if (err) throw err;
+
+      const lessons = JSON.parse(data);
+
+      res.render("home", { error: true, lessons: lessons });
+    });
+  } else {
+    fs.readFile("./data/lesson.json", (err, data) => {
+      if (err) throw err;
+      const lessons = JSON.parse(data);
+
+      const lesson = {
+        id: id(),
+        description: formData.lesson,
+        done: false,
+      };
+
+      lessons.push(lesson);
+
+      fs.writeFile("./data/lesson.json", JSON.stringify(lessons), (err) => {
+        if (err) throw err;
+
+        fs.readFile("./data/lesson.json", (err, data) => {
+          if (err) throw err;
+
+          const lessons = JSON.parse(data);
+
+          res.render("home", { success: true, lessons: lessons });
+        });
+      });
+    });
+  }
+});
+//delete button functions
+app.get("/:id/delete", (req, res) => {
+  //Saving the ID value
+  const id = req.params.id;
+
+  fs.readFile("./data/lesson.json", (err, data) => {
+    if (err) throw err;
+
+    const lessons = JSON.parse(data);
+
+    const filteredlessons = lessons.filter((lesson) => lesson.id != id);
+
+    fs.writeFile(
+      "./data/lesson.json",
+      JSON.stringify(filteredlessons),
+      (err) => {
+        if (err) throw err;
+
+        res.render("home", { lessons: filteredlessons, delete: true });
+      }
+    );
+  });
+});
+//updateing the existing note
+app.get("/:id/update", (req, res) => {
+  const id = req.params.id;
+
+  fs.readFile("./data/lesson.json", (err, data) => {
+    if (err) throw err;
+    //Finding the lesson to be changed
+    const lessons = JSON.parse(data);
+    const lesson = lessons.filter((lesson) => lesson.id == id)[0];
+
+    //Getting that lessons
+    const lessonIdx = lessons.indexOf(lesson);
+    const splicedlesson = lessons.splice(lessonIdx, 1)[0];
+
+    //Changing their status
+    splicedlesson.done = true;
+
+    //Adding back those lessons
+    lessons.push(splicedlesson);
+
+    fs.writeFile("./data/lesson.json", JSON.stringify(lessons), (err) => {
+      if (err) throw err;
+
+      res.render(`home`, { lessons: lessons });
+    });
+  });
+});
 
 app.listen(PORT, (err) => {
   if (err) throw err;
